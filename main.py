8a22f96b1a81f4161dea5359a473bcb93d8fc9b5
Ids Osinga <ip.osinga@alfa-college.nl>
2025-11-07 11:54:20 +0100

    case 2 geimplementeerd

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from unittest import case
 print("Welk type procentsom wil je oplossen; maak je keuze:")
 print("[1] Hoeveel is x% van y?")
 print("[2] Hoeveel % is x van y")
+print("[3] x neemt toe met y%; hoeveel heb je nu?")
 choice = input("Welke optie kies je?")
 match choice:
     case "1":
@@ -13,6 +14,13 @@ match choice:
         print("Het antwoord is: " + str(answer))
         print("De berekening is: " + str(x) + "% = " + str(x/100) + " en " + str(x/100) + " x " + str(y) + " = " + str(answer) )
     case "2":
-        print("optie 2 is gekozen")
+        x = float(input("Wat is x?"))
+        y = float(input("Wat is y?"))
+        answer = x / y * 100
+        print(f"De vraag is: hoeveel % is {x} van {y}?")
+        print(f"Het antwoord is: {answer}")
+        print(f"De berekening is: {x} : {y} = {x/y}; {x/y} x 100 = {answer:.0f}%")
+    case "3":
+
     case _:
         print("je hebt een ongeldige optie gekozen")
